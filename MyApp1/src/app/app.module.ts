0b7d9d20dc1ee50612c34be204fdf19dbdc29dd4
lucasrviana <lucasrviana@gmail.com>
2020-09-21 11:52:28 -0300

    MyApp

---
+++ MyApp1/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NomeComponenteComponent } from './pasta/nome-componente/nome-componente.component';
+import { HomeComponent } from './home/home.component';
+import { ServicoService } from './servico.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NomeComponenteComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [ServicoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
